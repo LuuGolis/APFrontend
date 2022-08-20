4ff704b139ccd95b2f1835df84a4c3cd753191d2
LuuGolis <golischevskyl@gmail.com>
2022-08-20 09:06:29 -0300

    Creación backend en netbeans 12.4

---
+++ ProyectoIntegrador/BACKEND/src/test/java/com/portfolio/glap/GlapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.glap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

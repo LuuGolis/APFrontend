93c36bfe5c665269c7fab385c7e826ec2d1a1cb4
LuuGolis <golischevskyl@gmail.com>
2022-08-25 12:03:14 -0300

    Security jwt

--- ProyectoIntegrador/BACKEND/src/main/java/com/portfolio/glap/Security/Service/RolService.java
+++ ProyectoIntegrador/BACKEND/src/main/java/com/portfolio/glap/Security/Service/RolService.java
@@ -5,10 +5,32 @@
  */
 package com.portfolio.glap.Security.Service;
 
+import com.portfolio.glap.Security.Entity.Rol;
+import com.portfolio.glap.Security.Enums.RolNombre;
+import com.portfolio.glap.Security.Repository.iRolRepository;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 /**
  *
  * @author lugol
  */
+@Service
+//si hay un error transactional hace un rollback y que no impacte en la db o se mantenga en un estado anterior (persistencia)
+@Transactional
 public class RolService {
+    @Autowired
+    iRolRepository irolRepository;
     
+    public Optional<Rol> getByRolNombre(RolNombre rolNombre){
+        
+        return irolRepository.findByRolNombre(rolNombre);
+    }
+    
+    public void save(Rol rol){
+        irolRepository.save(rol);
+    }
 }
+

e2811220b2f95694cdfc070329fa34ee03230f6b
LuuGolis <golischevskyl@gmail.com>
2022-08-25 09:34:21 -0300

    fix error de ayer + security package

--- ProyectoIntegrador/BACKEND/src/main/java/com/portfolio/glap/Security/Entity/UsuarioPrincipal.java
+++ ProyectoIntegrador/BACKEND/src/main/java/com/portfolio/glap/Security/Entity/UsuarioPrincipal.java
@@ -5,25 +5,20 @@
  */
 package com.portfolio.glap.Security.Entity;
 
-//buscar xq chota no carga 
+
 import java.util.Collection;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.userdetails.UserDetails;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 /**
  *
  * @author lugol
  */
-/*
-probar esto/leer comentarios del video
-<dependency>
-            <groupId>org.springframework.boot</groupId>
-            <artifactId>spring-boot-starter-security</artifactId>
-</dependency>
-<dependency>
-            <groupId>io.jsonwebtoken</groupId>
-            <artifactId>jjwt</artifactId>
-            <version>0.9.1</version>
-</dependency> */
+
+
 
 public class UsuarioPrincipal implements UserDetails{
     private String nombre;
@@ -32,5 +27,61 @@ public class UsuarioPrincipal implements UserDetails{
     private String password;
     private Collection<? extends GrantedAuthority> authorities;
     
+    //constructor
+
+    public UsuarioPrincipal(String nombre, String nombreUsuario, String email, String password, Collection<? extends GrantedAuthority> authorities) {
+        this.nombre = nombre;
+        this.nombreUsuario = nombreUsuario;
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+    }
+    
+    public static UsuarioPrincipal build(Usuario usuario){
+        List<GrantedAuthority> authorities = usuario.getRoles().stream().map(rol -> new SimpleGrantedAuthority(rol.getRolNombre().name())).collect(Collectors.toList());
+    
+    return new UsuarioPrincipal(usuario.getNombre(), usuario.getNombreUsuario(), usuario.getEmail(), usuario.getPassword(), authorities);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String getUsername() {
+        return nombreUsuario;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
     
 }
